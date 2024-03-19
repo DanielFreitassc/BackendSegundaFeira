284437d57e5df1ec6c5337e619fa13e3841a0634
Daniel Freitas <daniel.275622@alunosatc.edu.br>
2024-03-18 21:53:08 -0300

    troquei o nome das pastas

--- ExemplosDadoEmAula/minecraft/src/main/java/com/example/minecraft/models/ItemEntity.java
+++
@@ -1,9 +0,0 @@
-package com.example.minecraft.models;
-
-import lombok.Data;
-
-@Data
-public class ItemEntity {
-    private String nome = "Item padrao";
-}
-
---
+++ futuroExercicio/src/main/java/com/danielfeitassc/exercicio02/services/JogadorService.java
@@ -0,0 +1,71 @@
+package com.danielfeitassc.exercicio02.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.danielfeitassc.exercicio02.models.JogadorEntity;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JogadorService {
+    
+private static final String SOBRENOMES_URI = "https://venson.net.br/resources/data/sobrenomes.txt";
+    private static final String NOMES_URI = "https://venson.net.br/resources/data/nomes.txt";
+    private static final String CLUBES_URI = "https://venson.net.br/resources/data/clubes.txt";
+    private static final String POSICOES_URI = "https://venson.net.br/resources/data/posicoes.txt";
+
+    private List<String> sobrenomes;
+    private List<String> nomes;
+    private List<String> clubes;
+    private List<String> posicoes;
+    
+    public JogadorService() {
+        this.sobrenomes = fetchData(SOBRENOMES_URI);
+        this.nomes = fetchData(NOMES_URI);
+        this.clubes = fetchData(CLUBES_URI);
+        this.posicoes = fetchData(POSICOES_URI);
+    }
+
+    private List<String> fetchData(String urlString) {
+        List<String> data = new ArrayList<>();
+        try {
+            URI uri = new URI(urlString);
+            HttpURLConnection con = (HttpURLConnection) uri.toURL().openConnection();
+            con.setRequestMethod("GET");
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                in.lines().forEach(data::add);
+            }
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+    
+
+    public JogadorEntity generateJogador() {
+        Random random = new Random();
+        String nome = nomes.get(random.nextInt(nomes.size()));
+        String sobrenome = sobrenomes.get(random.nextInt(sobrenomes.size()));
+        String clube = clubes.get(random.nextInt(clubes.size()));
+        String posicao = posicoes.get(random.nextInt(posicoes.size()));
+        int idade = random.nextInt(20) + 18;
+        return new JogadorEntity(nome, sobrenome, posicao, idade, clube);
+    }
+
+    public List<JogadorEntity> generateJogadores(int quantity) {
+        List<JogadorEntity> jogadores = new ArrayList<>();
+        for (int i = 0; i < quantity; i++) {
+            jogadores.add(generateJogador());
+        }
+        return jogadores;
+    }
+}
+

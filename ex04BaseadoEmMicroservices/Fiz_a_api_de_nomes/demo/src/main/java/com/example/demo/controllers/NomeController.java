88c46aac993fee6e64173c38afc7e5f2c28e40c8
DanielFreitassc <danielfreitassc@gmail.com>
2024-04-01 14:31:24 -0300

    Microservices

---
+++ ex04BaseadoEmMicroservices/Fiz_a_api_de_nomes/demo/src/main/java/com/example/demo/controllers/NomeController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("jogador")
+public class NomeController {
+    private static final String BASE_PATH = "src/main/resources/static/";
+
+    @GetMapping("/{fileName}")
+    public List<String> listarConteudoArquivo(@PathVariable String fileName) {
+        return lerArquivo(fileName);
+    }
+
+    private List<String> lerArquivo(String fileName) {
+        List<String> content = new ArrayList<>();
+        try {
+            content.addAll(Files.readAllLines(Paths.get(BASE_PATH + fileName)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao ler o arquivo " + fileName + ": " + e.getMessage());
+        }
+        return content;
+    }
+
+}
